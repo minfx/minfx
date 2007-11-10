36238e8ebce4bc4f5f943caf69c6e57d93ac299b
Edward d'Auvergne <edward@nmr-relax.com>
2007-11-10 22:33:27 +0000

    Added the GPL to the distutils metadata.

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from distutils.core import setup
 
+
+# Setup for distutils.
 setup(name='minfx',
       version='trunk',
       description='The minfx optimisation library',
       author="Edward d'Auvergne",
       author_email='edward@nmr-relax.com',
       url='https://gna.org/projects/minfx/',
-      packages=['minfx', 'minfx.line_search', 'minfx.hessian_mods'],
-      )
+      license='GPL',
+      packages=['minfx', 'minfx.line_search', 'minfx.hessian_mods'])
