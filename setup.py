df3bc0d6ca22f2140d3d4e226b22027609302d89
Edward d'Auvergne <edward@nmr-relax.com>
2007-11-10 19:34:51 +0000

    Added a bit more to the project description.

--- setup.py
+++ setup.py
@@ -2,5 +2,9 @@ from distutils.core import setup
 
 setup(name='minfx',
       version='trunk',
+      description='The minfx optimisation library',
+      author="Edward d'Auvergne",
+      author_email='edward@nmr-relax.com',
+      url='https://gna.org/projects/minfx/',
       py_modules=['minfx'],
       )
