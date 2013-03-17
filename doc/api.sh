f103dcca727361e3a57b4bbf3a20cee21496ecd2
Edward d'Auvergne <edward@nmr-relax.com>
2013-03-17 09:35:06 +0000

    Removed the name 'relax' from the minfx API documentation creation script.
    This is a remnant from the project relax (http://www.nmr-relax.com).

--- doc/api.sh
+++ doc/api.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
 # Shell script for building the API documentation.
-epydoc --html -o doc/api --docformat epytext --css white --name relax --url https://gna.org/projects/minfx --show-frames --show-private --show-imports -v --graph all --show-sourcecode --exclude=devel_scripts --exclude=extern --exclude=graphics --exclude=minfx.scipy_subset minfx
+epydoc --html -o doc/api --docformat epytext --css white --name minfx --url https://gna.org/projects/minfx --show-frames --show-private --show-imports -v --graph all --show-sourcecode --exclude=devel_scripts --exclude=extern --exclude=graphics --exclude=minfx.scipy_subset minfx
